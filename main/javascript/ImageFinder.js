0f393e3550e7654d85f0a28505a2b64deb672085
kolesan <kolesan90@gmail.com>
2019-03-15 19:59:37 +0200

    1st task (query the static db)

--- main/javascript/ImageFinder.js
+++ main/javascript/ImageFinder.js
@@ -3,20 +3,17 @@
   var ImageFinder = window.CLASSES.ImageFinder = function () {};
 
   ImageFinder.prototype.search = function (query) {
+    let images = DATA.staticImagesDb
+      .filter(image => image.title.includes(query))
+      .map(image => ({
+        id: image.id,
+        url: image.url,
+        title: image.title
+      }));
+
     return {
-      query: 'demo',
-      images: [
-        {
-          id:'1',
-          url:'http://image.shutterstock.com/display_pic_with_logo/347836/99127196/stock-photo-demo-icon-99127196.jpg',
-          title:'demo image 1'
-        },
-        {
-          id:'2',
-          url:'http://t2.ftcdn.net/jpg/00/30/42/21/400_F_30422159_lzSKGlGNX1YcKGuIFDiEyZbmCF3hacIB.jpg',
-          title:'demo image 2'
-        }
-      ]
+      query,
+      images
     };
   }
 
